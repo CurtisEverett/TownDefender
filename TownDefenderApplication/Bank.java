4d40c0eca8357cc891dc955d9d8a1cf5ee346adc
CurtisEverett <61245280+CurtisEverett@users.noreply.github.com>
2020-11-30 17:17:32 +0100

    First push to initialize

---
+++ TownDefenderApplication/Bank.java
@@ -0,0 +1,47 @@
+
+public class Bank
+{
+    private static int money;
+    private static int roundmoney;
+    
+    private static boolean setMoney(int newMoney)
+    {
+        try{
+            money=newMoney;
+            return true;
+        }
+        catch(Exception e)
+        {
+            return false;
+        } 
+    }
+    
+    public static boolean minus(int soll)
+    {
+        try{
+            money-=soll;
+            return true;
+        }
+        catch(Exception e)
+        {
+            return false;
+        } 
+    }
+    
+    public static boolean plus(int haben)
+    {
+        try{
+            money+=haben;
+            return true;
+        }
+        catch(Exception e)
+        {
+            return false;
+        } 
+    }
+    
+    public static int getMoney()
+    {
+        return money;
+    }
+}
